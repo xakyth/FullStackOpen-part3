52b5f1dc1994c655e3b5f086bc0106d3c441cf88
xakyth <xakyth@gmail.com>
2023-11-06 14:56:34 +0300

    added static dir

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ morgan.token('body', (req, res) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
