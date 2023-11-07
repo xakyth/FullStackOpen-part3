d0e64837e48a461742041141676201714de73f06
xakyth <xakyth@gmail.com>
2023-11-07 20:52:46 +0300

    done ex. 3.15-3.18

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const person = require('./models/person')
 
 const app = express()
 
 app.use(express.json())
 morgan.token('body', (req, res) => { 
-    if (req.method === 'POST')
-        return JSON.stringify(req.body)
-    else
-        return null
+    return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
@@ -23,13 +21,14 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        console.log('person', person)
-        response.json(person)
+        if (person)
+            response.json(person)
+        else
+            response.status(404).end()
     }).catch(error => {
-        console.log('error', error)
-        response.status(404).end()
+        next(error)
     })
 })
 app.get('/info', (request, response) => {
@@ -40,12 +39,29 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.deleteOne({ _id: request.params.id}).then(resp => {
-        console.log('resp', resp)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id).then(resp => {
         response.status(204).end()
-    })
+    }).catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const updPerson = {
+        name: request.body.name,
+        number: request.body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, updPerson, { new: true }).then(retPerson => {
+        response.json(retPerson)
+    }).catch(error => next(error))
+})
+
+const errorHandler = (err, req, res, next) => {
+    console.error('err.message', err.message)
+    if (err.name === 'CastError')
+        res.status(400).send({ error: 'malformed id' })
+    next(err)
+}
+app.use(errorHandler)
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
